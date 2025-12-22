cf9b5052202bd979aaebfc616ce6c8d6186f5e52
Babafemi Ajayi <babafemiajayi00@gmail.com>
2025-12-22 18:27:31 +0000

    Linux

--- log_parser/CODE/Activity1_log_reader.py
+++ log_parser/CODE/Activity1_log_reader.py
@@ -1,9 +1,16 @@
+storedlist = []
+
+file_path = "Sample_Logs/auth.log"
+
+with open(file_path, "r") as file:
+    for lines in file:
+        storedlist.append(lines.strip())
+
+        print("The total number of entries is: {storedlist}")
+        print("The first entry is: {storedlist}[0]")
+        print("The last entry is: {storedlist}[-1]")
 
 
-file_path = "sample_logs/auth.log"
-
-with open: (file_path, "r")as file:
-    for line in file:
 
 
 
